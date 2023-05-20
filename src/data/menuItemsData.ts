ad3b5a125898f6f54a3d4623e0cb8b475b5b8a4e
jmdiaz23 <jorgemariodiaz7@gmail.com>
2023-05-20 11:07:39 -0500

    agregando proyecto

---
+++ src/data/menuItemsData.ts
@@ -0,0 +1,22 @@
+import type { MenuItem } from "../types/types";
+
+const menuItems: MenuItem[]=[
+    {
+        title: "Home",
+        url: "/",
+    },
+    {
+        title:"Services",
+        url:"/services",
+    },
+    {
+        title: "Blog",
+        url:"/blog",
+    },
+    {
+        title: "Contact",
+        url:"/contact",
+    },
+];
+
+export default menuItems;
